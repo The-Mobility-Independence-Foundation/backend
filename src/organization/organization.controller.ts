2b845fd1ecc7a66603f66f3a95b84e900c703a96
aas1984 <aas1984@g.rit.edu>
2025-01-19 15:53:26 -0500

    Added initial Organization module

---
+++ src/organization/organization.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Param, Post, Get } from '@nestjs/common';
+import { OrganizationService } from './organization.service';
+import { Organization } from './organization.entity';
+
+@Controller('organization')
+export class OrganizationController {
+
+    constructor(private organizationService: OrganizationService) {}
+    
+    @Post()
+    create(): Promise<Organization> {
+        return this.organizationService.create();
+    }
+
+    @Get()
+    findAll(): Promise<Organization[]> {
+        return this.organizationService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<Organization | null> {
+        return this.organizationService.findOne(id);
+    }
+
+}
