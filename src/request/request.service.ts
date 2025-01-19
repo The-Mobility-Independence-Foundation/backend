6915b150ae51f7ac56d2470f317088fe44365112
aas1984 <aas1984@g.rit.edu>
2025-01-19 16:45:25 -0500

    Added initial Request module

---
+++ src/request/request.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Request } from './request.entity';
+
+@Injectable()
+export class RequestService {
+
+    constructor(
+        @InjectRepository(Request)
+        private requestRepository: Repository<Request>,
+    ) {}
+
+    async create() {
+        const request = new Request();
+        request.ein = 920887459;
+        request.firstName = "Johnathan";
+        request.lastName = "Test";
+        request.email = "johntest@yahoo.gov";
+        request.description = "Let me in!";
+
+        return this.requestRepository.save(request);
+    }
+
+    async findAll() {
+        
+        return this.requestRepository.find();
+        
+    }
+
+    async findOne(id: number) {
+
+        return this.requestRepository.findOneBy({requestID: id});
+
+    }
+
+}
