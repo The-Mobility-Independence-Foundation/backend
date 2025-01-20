19974cbe6799bd32db06b1c44dfaed46bf3f6b5b
aas1984 <aas1984@g.rit.edu>
2025-01-20 13:16:09 -0500

    Added relation between User and Organization

--- src/organization/organization.entity.ts
+++ src/organization/organization.entity.ts
@@ -1,56 +1,60 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/user/user.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 @Entity()
 export class Organization {
 
     @PrimaryGeneratedColumn()
-    organizationID: number
+    organizationID: number;
 
     @Column() // TODO: set foreign key on inventory NOT NULL
-    inventoryID: number 
+    inventoryID: number;
 
     @Column() // TODO: set foreign key on user NOT NULL
-    userID: number 
+    userID: number;
 
     // returns a 500 server error unless we catch the duplicate name error ourselves. 
     // organizationID will still increment if a unique name is sent though, leaving us with blank rows
     @Column({ type: "varchar", length: 50 })
-    name: string 
+    name: string;
 
     @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
-    lastActivity: number
+    lastActivity: number;
 
     @Column({ default: false })
-    inactive: boolean 
+    inactive: boolean;
 
     @Column({ type: "varchar", length: 4000, default: "" })
-    services: string 
+    services: string;
 
     @Column({ type: 'decimal', default: 0.0 })
-    rating: number
+    rating: number;
 
     @Column({ type: 'varchar', length: 50 })
-    address1: string
+    address1: string;
 
     @Column({ type: 'varchar', length: 50, default: "" })
-    address2: string
+    address2: string;
 
     @Column({ type: 'varchar', length: 30 })
-    city: string
+    city: string;
 
     @Column({ type: 'varchar', length: 15 })
-    state: string
+    state: string;
 
     @Column()
-    zipcode: number
+    zipcode: number;
 
     @Column({ type: 'varchar', length: 20, nullable: true })
-    phoneNumber: string
+    phoneNumber: string;
 
     @Column()
-    ein: number
+    ein: number;
 
     @Column({ type: "varchar", length: 50, array: true, nullable: true })
     socials: string[];
+
+    @OneToMany(() => User, (user) => user.organization)
+    members: User[];
 
 }
