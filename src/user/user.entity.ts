b22bbb240330493a9e3bffd497b27b1729cac203
aas1984 <aas1984@g.rit.edu>
2025-01-26 15:34:08 -0500

    feat: added connections table through a relation

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -99,4 +99,11 @@ export class User {
   @JoinTable({ name: "bookmarks" })
   @ManyToMany(() => Listing, listing => listing.bookmarks)
   bookmarks: Listing[];
+
+  @JoinTable({ name: "connections" })
+  @ManyToMany(() => User, user => user.connectionsRecieved)
+  connectionsSent: User[];
+
+  @ManyToMany(() => User, user => user.connectionsSent)
+  connectionsRecieved: User[];
 }
