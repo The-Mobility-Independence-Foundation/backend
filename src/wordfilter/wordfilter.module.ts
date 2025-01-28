4249bd4b563132ed72a4f08d499f6bbaa81c268f
Jaime Diaz <jsd5582@rit.edu>
2025-01-27 20:52:49 -0500

    Fixes to word filter module

--- src/wordfilter/wordfilter.module.ts
+++ src/wordfilter/wordfilter.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WordfilterController } from './wordfilter.controller';
 import { WordfilterService } from './wordfilter.service';
+import { Wordfilter } from './wordfilter.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Wordfilter])],
   controllers: [WordfilterController],
   providers: [WordfilterService]
 })
