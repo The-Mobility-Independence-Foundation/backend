38a61ac43fa53ee2aec14064688f279adf0c36cd
Jaime Diaz <jsd5582@rit.edu>
2025-01-28 20:48:54 -0500

    creation of the forum module

---
+++ src/forum/forum.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ForumController } from './forum.controller';
+import { ForumService } from './forum.service';
+import { Forum } from './forum.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Forum])],
+  controllers: [ForumController],
+  providers: [ForumService]
+})
+export class ForumModule {}
