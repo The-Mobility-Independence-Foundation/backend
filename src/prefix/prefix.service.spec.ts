efdf5347002f74a7083e2f54caf86d07617752f6
Jaime Diaz <jsd5582@rit.edu>
2025-02-01 15:16:15 -0500

    test fixed to prefix

--- src/prefix/prefix.service.spec.ts
+++ src/prefix/prefix.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { PrefixService } from './prefix.service';
+import {Prefix} from './prefix.entity';
 
 describe('PrefixService', () => {
   let service: PrefixService;
 
+  const mockPrefixRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+    save: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  const mockForumRepository = {
+    find: jest.fn(),
+    findOne: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrefixService],
+      providers: [
+        PrefixService,
+        {
+          provide: getRepositoryToken(Prefix),
+          useValue: mockPrefixRepository, // Mocking PrefixRepository
+        },
+        {
+          provide: 'ForumRepository', // If it's injected as @Inject('ForumRepository')
+          useValue: mockForumRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<PrefixService>(PrefixService);
