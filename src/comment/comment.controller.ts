7d1e5a7e9547728d1f78e33ee77fe137619617c4
Jaime Diaz <jsd5582@rit.edu>
2025-02-01 17:23:06 -0500

    Comment module complete

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Param } from '@nestjs/common';
+import { Comment } from './comment.entity';
+import { CommentService } from './comment.service';
 
 @Controller('comment')
-export class CommentController {}
+export class CommentController {
+    constructor(private readonly commentService: CommentService) {}
+
+    @Post()
+    create(): Promise<Comment> {
+        return this.commentService.create();
+    }
+
+    @Get()
+    findAll(): Promise<Comment[]> {
+        return this.commentService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<Comment | null> {
+        return this.commentService.findOne(id);
+    }
+}
