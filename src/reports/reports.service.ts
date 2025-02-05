c19d87d20d0ee01df0a22a685439440b3f0eecf9
Jaime Diaz <jsd5582@rit.edu>
2025-02-04 20:30:03 -0500

    feat: finishing touches on reports

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Post as PostEntity} from '../post/post.entity';
+import { Post as PostEntity } from '../post/post.entity';
 import { User } from '../user/user.entity';
 import { Listing } from '../listing/listing.entity';
 import { Report, ReportType } from './report.entity';
 
 @Injectable()
 export class ReportsService {
-    constructor(
-        @InjectRepository(Report)
-        private reportRepository: Repository<Report>,
+  constructor(
+    @InjectRepository(Report)
+    private reportRepository: Repository<Report>,
 
-        @InjectRepository(PostEntity)
-        private postRepository: Repository<PostEntity>,
-    
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-    
-        @InjectRepository(Listing)
-        private listingRepository: Repository<Listing>,
-      ) {}
-    
-    async create() {
+    @InjectRepository(PostEntity)
+    private postRepository: Repository<PostEntity>,
+
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+
+    @InjectRepository(Listing)
+    private listingRepository: Repository<Listing>,
+  ) {}
+
+  async create() {
     const report = new Report();
 
     const reporter = await this.userRepository.findOneBy({ id: 1 });
@@ -36,14 +36,14 @@ export class ReportsService {
       report.listing = listing;
     }
     if (respondent) {
-        report.respondent = respondent;
+      report.respondent = respondent;
     }
 
     report.reason = 'fake profile';
     report.type = ReportType.LISTING;
 
     return this.reportRepository.save(report);
-  }  
+  }
 
   async findAll() {
     return this.reportRepository.find();
