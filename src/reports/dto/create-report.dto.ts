06d03e25d7fed40063d8df8f19bd0d77abd59c50
aas1984 <aas1984@g.rit.edu>
2025-03-01 16:42:55 -0500

    feat: added CreateReportDto

---
+++ src/reports/dto/create-report.dto.ts
@@ -0,0 +1,60 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ReportType } from '../report.entity';
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @ApiProperty({ description: 'The id of the user who made the report.' })
+  @IsPositive()
+  @IsInt()
+  reporterId: number;
+
+  @ApiProperty({ description: 'The id of the user who is being reported.' })
+  @IsPositive()
+  @IsInt()
+  reportedUserId: number;
+
+  @IsOptional()
+  @ApiProperty({
+    description:
+      "The id of the post being reported, if ReportType = 'post'. Else, null.",
+  })
+  @IsPositive()
+  @IsInt()
+  postId?: number | null;
+
+  @IsOptional()
+  @ApiProperty({
+    description:
+      "The id of the listing being reported, if ReportType = 'listing'. Else, null.",
+  })
+  @IsPositive()
+  @IsInt()
+  listingId?: number | null;
+
+  @IsOptional()
+  @ApiProperty({
+    description:
+      "The id of the comment being reported, if ReportType = 'comment'. Else, null.",
+  })
+  @IsPositive()
+  @IsInt()
+  commentId?: number | null;
+
+  @ApiProperty({ description: "The user's reason for creating the report." })
+  @IsNotEmpty()
+  @MaxLength(200)
+  reason: string;
+
+  @ApiProperty({
+    description: 'An enum that determines what type of report this is.',
+  })
+  @IsEnum(ReportType)
+  reportType: ReportType;
+}
