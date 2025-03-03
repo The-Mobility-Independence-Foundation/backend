829a19748ffaa66aa6946853024e8db7c62125e0
jon <70080228+notcruz@users.noreply.github.com>
2025-03-02 20:05:38 -0500

    feat: core auth workflow complete

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,18 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { UserValidation } from '../../common/validation/user.validation';
+import { IsEmail, IsPassword } from '../../common/decorators/user.decorators';
 
 /**
  * The login data (through local auth)
  */
 export class LoginDto {
   @IsEmail()
-  @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    minLength: UserValidation.email.min,
+    maxLength: UserValidation.email.max,
+  })
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(12)
-  @ApiProperty()
+  @IsPassword()
+  @ApiProperty({
+    example: 'StrongP@ssw0rd',
+    minLength: UserValidation.password.min,
+    maxLength: UserValidation.password.max,
+  })
   password: string;
 }
