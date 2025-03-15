5e3fa88cbc8814ec6b5c16eb6967ecca11b69131
aas1984 <aas1984@g.rit.edu>
2025-03-15 17:59:06 -0400

    refactor: moved report tests to dedicated folder

--- src/reports/__tests__/reports.service.spec.ts
+++ src/reports/__tests__/reports.service.spec.ts
@@ -1,11 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Listing } from '../listing/listing.entity';
-import { Post as PostEntity } from '../post/post.entity';
-import { User } from '../user/entities/user.entity';
-import { Report } from './report.entity';
-import { ReportsService } from './reports.service';
-import { Comment } from '../comment/comment.entity';
+import { Listing } from '../../listing/listing.entity';
+import { Post as PostEntity } from '../../post/post.entity';
+import { User } from '../../user/entities/user.entity';
+import { Report } from '../report.entity';
+import { ReportsService } from '../reports.service';
+import { Comment } from '../../comment/comment.entity';
 
 export const mockRepository = jest.fn(() => ({
   metadata: {
