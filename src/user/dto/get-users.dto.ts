496b5a8448733a99f966a537bd3dbf6d3e0c5c91
aas1984 <aas1984@g.rit.edu>
2025-03-19 13:53:21 -0400

    feat: handled comments from PR
    Replaced BadRequestExceptions with NotFoundExceptions when appropriate.
    Removed Rating from the dtos, service, and tests (this feature is no longer MVP)

--- src/user/dto/get-users.dto.ts
+++ src/user/dto/get-users.dto.ts
@@ -1,5 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsBoolean, IsOptional, IsPositive, Max, Min } from 'class-validator';
+import { IsBoolean, IsOptional, IsPositive } from 'class-validator';
 import { UserRole } from '../entities/user.entity';
 
 export class GetUsersDto {
@@ -12,18 +12,6 @@ export class GetUsersDto {
   @ApiPropertyOptional()
   @IsPositive()
   count?: number;
-
-  @IsOptional()
-  @ApiPropertyOptional()
-  @Min(0, { message: 'Minimum rating must be greater than 0.0' })
-  @Max(5, { message: 'Minimum rating must be less than 5.0' })
-  minRating?: number;
-
-  @IsOptional()
-  @ApiPropertyOptional()
-  @Min(0, { message: 'Maximum rating must be greater than 0.0' })
-  @Max(5, { message: 'Maximum rating must be less than 5.0' })
-  maxRating?: number;
 
   @IsOptional()
   @ApiPropertyOptional()
