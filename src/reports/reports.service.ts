6a794ef28909ef4d6b7fd763ac22970ee3b55a77
aas1984 <aas1984@g.rit.edu>
2025-03-20 16:17:07 -0400

    refactor: made some changes based on code review
    All of these changes are based off of comments from /users, as /reports hasn't had comments yet.
    
    * Added findById to reports.service
    * Replaced many BadRequestExceptions with NotFoundExceptions
    * Added pagination
    * Added JWT Auth guards to controller
    * Replaced some if statements with Object.Assign()
    * Added ResponseMessage and ApiResponse decorators to constructor

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -56,6 +56,7 @@ export class ReportsService {
       throw new BadRequestException('You cannot report yourself.');
     }
 
+    // TODO: once user is merged, update with findById
     const reporter = await this.userRepository.findOneBy({
       id: dto.reporterId,
     });
