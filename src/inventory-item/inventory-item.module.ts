46f040d4e7af2cebf1899fd737340abae7180aed
Jaime Diaz <jsd5582@rit.edu>
2025-03-22 16:45:39 -0400

    feat: pagination to findAll, using get dto, object assign for update

--- src/inventory-item/inventory-item.module.ts
+++ src/inventory-item/inventory-item.module.ts
@@ -3,20 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { InventoryItemController } from './inventory-item.controller';
 import { InventoryItemService } from './inventory-item.service';
 import { InventoryItem } from './inventory-item.entity';
-import { Inventory } from '../inventory/inventory.entity';
 import { Manufacturer } from '../model/model.entity';
 import { ModelModule } from '../model/model.module';
 import { PartModule } from '../part/part.module';
 import { TagModule } from '../tag/tag.module';
+import { CommonModule } from '../common/common.module';
+import { InventoryModule } from '../inventory/inventory.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([InventoryItem, Inventory, Manufacturer]),
+    TypeOrmModule.forFeature([InventoryItem, Manufacturer]),
     ModelModule,
     PartModule,
     TagModule,
+    CommonModule,
+    InventoryModule,
   ],
   controllers: [InventoryItemController],
   providers: [InventoryItemService],
+  exports: [InventoryItemService],
 })
 export class InventoryItemModule {}
--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -16,5 +16,6 @@ import { CommonModule } from '../common/common.module';
   ],
   controllers: [InventoryController],
   providers: [InventoryService],
+  exports: [InventoryService],
 })
 export class InventoryModule {}
