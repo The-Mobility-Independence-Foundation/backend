47ad5f6a7957fd5899fb9ccf69240be9db5e4f8c
Jaime Diaz <jaimediaz387@gmail.com>
2025-03-29 14:11:36 -0400

    feat: changed controller modelType -> model-type

--- src/model-type/model-type.controller.ts
+++ src/model-type/model-type.controller.ts
@@ -17,9 +17,9 @@ import { GetModelTypesDto } from './dto/get-model-type.dto';
 import { BaseApiCursorPaginationResponse } from '../common/responses/base-api-cursor-pagination.response';
 import { UpdateModelTypeDto } from './dto/update-model-type.dto';
 
-@ApiTags('modelType')
+@ApiTags('model-type')
 @UseStrategy(ResourceAccessStrategyToken.PUBLIC_USER)
-@Controller('modelType')
+@Controller('model-type')
 export class ModelTypeController {
   constructor(private readonly modelTypeService: ModelTypeService) {}
 
