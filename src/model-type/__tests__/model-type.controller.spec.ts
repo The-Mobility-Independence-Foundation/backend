7d6c11f4080c1c99c207fde41f9df589f75c9d02
Jaime Diaz <jaimediaz387@gmail.com>
2025-03-29 09:30:16 -0400

    feat: model type controller complete

--- src/model-type/__tests__/model-type.controller.spec.ts
+++ src/model-type/__tests__/model-type.controller.spec.ts
@@ -1,4 +1,6 @@
+import { createMock } from '@golevelup/ts-jest';
 import { Test, TestingModule } from '@nestjs/testing';
+import { STRATEGY_PROVIDERS_TOKEN, ResourceAccessStrategyRegistry } from '../../common/resource-access/interfaces/strategy-provider.interface';
 import { ModelTypeController } from '../model-type.controller';
 
 describe('ModelTypeController', () => {
@@ -7,7 +9,15 @@ describe('ModelTypeController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ModelTypeController],
-    }).compile();
+      providers: [
+        {
+          provide: STRATEGY_PROVIDERS_TOKEN,
+          useValue: createMock<ResourceAccessStrategyRegistry>(),
+        },
+      ],
+    })
+    .useMocker(createMock)
+    .compile();
 
     controller = module.get<ModelTypeController>(ModelTypeController);
   });
--- src/model-type/model-type.controller.ts
+++ src/model-type/model-type.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UseStrategy } from '../common/resource-access/decorators/resource-access.decorator';
+import { ResourceAccessStrategyToken } from '../common/resource-access/interfaces/strategy-provider.interface';
+import { ModelTypeService } from './model-type.service';
+import { ModelType } from '../model/model.entity';
+import { CreateModelTypeDto } from './dto/create-model-type.dto';
+import { GetModelTypesDto } from './dto/get-model-type.dto';
+import { BaseApiCursorPaginationResponse } from '../common/responses/base-api-cursor-pagination.response';
+import { UpdateModelTypeDto } from './dto/update-model-type.dto';
 
+@ApiTags('model-type')
+@UseStrategy(ResourceAccessStrategyToken.PUBLIC_USER)
 @Controller('model-type')
-export class ModelTypeController {}
+export class ModelTypeController {
+    constructor(private readonly modelTypeService: ModelTypeService) {}
+
+    @Post()
+    @ApiOperation({ summary: 'Initate creation of a model type' })
+    create(@Body() dto: CreateModelTypeDto): Promise<ModelType> {
+        return this.modelTypeService.create(dto);
+    }
+
+    @Get()
+    @ApiOperation({ summary: 'Retrieve a list of all model types' })
+    findAll(@Query() query: GetModelTypesDto): Promise<BaseApiCursorPaginationResponse<ModelType>> {
+        return this.modelTypeService.findAll(query);
+    }
+
+    @Get(':id')
+    @ApiOperation({ summary: 'Retrieve a specific model type' })
+    findOne(@Param('id') id: number): Promise<ModelType> {
+        return this.modelTypeService.findByIdOrThrow(id);
+    }
+
+    @Patch(':id')
+    @ApiOperation({ summary: 'Update information about a model type' })
+    @UseStrategy(ResourceAccessStrategyToken.PUBLIC_USER, { adminOnly: true })
+    update(@Param('id') id: number, @Body() dto: UpdateModelTypeDto) {
+        return this.modelTypeService.update(id, dto);
+    }
+}
