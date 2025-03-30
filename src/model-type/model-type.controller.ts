523ba2758f639bb896f4ec1f67a84c2db609bf02
Jaime Diaz <jaimediaz387@gmail.com>
2025-03-29 21:28:54 -0400

    feat: added response messages to all endpoints

--- src/model-type/model-type.controller.ts
+++ src/model-type/model-type.controller.ts
@@ -16,6 +16,7 @@ import { CreateModelTypeDto } from './dto/create-model-type.dto';
 import { GetModelTypesDto } from './dto/get-model-type.dto';
 import { BaseApiCursorPaginationResponse } from '../common/responses/base-api-cursor-pagination.response';
 import { UpdateModelTypeDto } from './dto/update-model-type.dto';
+import { ResponseMessage } from '../common/decorators/response-message.decorator';
 
 @ApiTags('model-types')
 @UseStrategy(ResourceAccessStrategyToken.PUBLIC_USER)
@@ -24,12 +25,14 @@ export class ModelTypeController {
   constructor(private readonly modelTypeService: ModelTypeService) {}
 
   @Post()
+  @ResponseMessage('Successfully created a model type')
   @ApiOperation({ summary: 'Initate creation of a model type' })
   create(@Body() dto: CreateModelTypeDto): Promise<ModelType> {
     return this.modelTypeService.create(dto);
   }
 
   @Get()
+  @ResponseMessage('Successfully retreived all model types')
   @ApiOperation({ summary: 'Retrieve a list of all model types' })
   findAll(
     @Query() query: GetModelTypesDto,
@@ -38,12 +41,14 @@ export class ModelTypeController {
   }
 
   @Get(':id')
+  @ResponseMessage('Successfully retrieved a model type')
   @ApiOperation({ summary: 'Retrieve a specific model type' })
   findOne(@Param('id') id: number): Promise<ModelType> {
     return this.modelTypeService.findByIdOrThrow(id);
   }
 
   @Patch(':id')
+  @ResponseMessage('Successfully updated a model type')
   @ApiOperation({ summary: 'Update information about a model type' })
   @UseStrategy(ResourceAccessStrategyToken.PUBLIC_USER, { adminOnly: true })
   update(@Param('id') id: number, @Body() dto: UpdateModelTypeDto) {
