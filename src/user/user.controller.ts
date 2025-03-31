1c20cfff031230653444c7b79c569066afaab56f
aas1984 <aas1984@g.rit.edu>
2025-03-31 15:57:25 -0400

    fix: fixed pipe error with user GET
    I'm not confident this will continue to work but it works for now

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -32,23 +32,23 @@ export class UserController {
     return req.user as User;
   }
 
-  @Get()
+  @Get('/')
   @ResponseMessage('Successfully retrieved all users matching your criteria')
   @ApiOperation({ summary: 'Get all users matching search criteria' })
   async findAll(@Query() query: GetUsersDto) {
     return this.userService.findAll(query);
   }
 
-  @Get(':userId')
+  @Get('/:userId')
   @ResponseMessage('Successfully found user')
   @ApiOperation({ summary: 'Find a specific user given their id' })
-  async findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.findByIdOrThrow(id, {
+  async findOne(@Param('userId', ParseIntPipe) userId: number) {
+    return this.userService.findByIdOrThrow(userId, {
       relations: { organization: true },
     });
   }
 
-  @Patch(':userId')
+  @Patch('/:userId')
   @ResponseMessage('Successfully updated user')
   @ApiOperation({ summary: 'Update a specific user given their id' })
   @UseStrategy(ResourceAccessStrategyToken.USER)
