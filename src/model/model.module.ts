5b2cafcc13c028dc0f66bb45f5e5c77a51c2a242
Jaime Diaz <jaimediaz387@gmail.com>
2025-03-31 19:56:53 -0400

    feat: fucking forgot to remove the actual import at the top

--- src/model/model.module.ts
+++ src/model/model.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ModelService } from './model.service';
 import { ModelController } from './model.controller';
-import { Manufacturer, Model, ModelType } from './model.entity';
+import { Model } from './model.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from '../common/common.module';
 import { ModelTypeModule } from '../model-type/model-type.module';
