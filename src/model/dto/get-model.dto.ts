1a22c064985e7ad800acf1e063347c588abd39ff
Jaime Diaz <jaimediaz387@gmail.com>
2025-04-02 17:27:22 -0400

    refactor: added partial type to use create dto

--- src/model/dto/get-model.dto.ts
+++ src/model/dto/get-model.dto.ts
@@ -2,4 +2,7 @@ import { IntersectionType, PartialType } from '@nestjs/swagger';
 import { CursorPaginationDto } from '../../common/dto/cursor-pagination.dto';
 import { CreateModelDto } from './create-model.dto';
 
-export class GetModelsDto extends IntersectionType(CursorPaginationDto, PartialType(CreateModelDto)) {}
+export class GetModelsDto extends IntersectionType(
+  CursorPaginationDto,
+  PartialType(CreateModelDto),
+) {}
