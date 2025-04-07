ad3bf1d9aea4aaed318b860669d1bcd6e8f2a89a
Jaime Diaz <jaimediaz387@gmail.com>
2025-04-07 15:50:58 -0400

    feat: findByIdOrThrow tests made

--- src/request/__tests__/request.service.spec.ts
+++ src/request/__tests__/request.service.spec.ts
@@ -7,6 +7,8 @@ import { Repository } from 'typeorm';
 //import { PaginationService } from 'src/common/services/pagination.service';
 //import { UserService } from 'src/user/user.service';
 import { CreateRequestDto } from '../dto/create-request.dto';
+import { NotFoundException } from '@nestjs/common';
+import { when } from 'jest-when';
 
 describe('RequestService', () => {
   let service: RequestService;
@@ -50,9 +52,37 @@ describe('RequestService', () => {
         description: 'We want to help',
       });
 
-
       await expect(service.create(createDto)).resolves.not.toThrow();
       expect(requestRepository.save).toHaveBeenCalled();
+    });
+  });
+
+  describe('findByIdOrThrow', () => {
+    it('should throw an error when given a bad id', async () => {
+      const bad_id = 999999999;
+
+      when(requestRepository.findOne)
+        .calledWith({ where: { id: bad_id } })
+        .mockResolvedValue(null);
+
+      await expect(service.findByIdOrThrow(bad_id)).rejects.toThrow(
+        new NotFoundException('Request not found'),
+      );
+    });
+
+    it('should return the request when id is valid', async () => {
+      const request = new Request();
+      Object.assign(request, {
+        id: 1,
+      });
+
+      when(requestRepository.findOne)
+        .calledWith({ where: { id: request.id } })
+        .mockResolvedValue(request);
+
+      const result = await service.findByIdOrThrow(request.id);
+
+      expect(result).toBeDefined();
     });
   });
 });
