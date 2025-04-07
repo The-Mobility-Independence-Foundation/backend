4fc60e90cae008e1811d040ac245d1b5483b4804
Jaime Diaz <jaimediaz387@gmail.com>
2025-04-07 13:45:02 -0400

    feat: create, update, get dtos are complete

--- src/request/dto/create-request.dto.ts
+++ src/request/dto/create-request.dto.ts
@@ -2,51 +2,48 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateRequestDto {
-    @ApiProperty()
-    @IsNotEmpty({ message: 'Name can not be empty' })
-    @MaxLength(50, {
-        message:
-          'Name is too long. Maximum length is $constraint1 characters.',
-      })
-    name: string;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Name can not be empty' })
+  @MaxLength(50, {
+    message: 'Name is too long. Maximum length is $constraint1 characters.',
+  })
+  name: string;
 
-    @ApiProperty()
-    @IsNotEmpty({ message: 'First name can not be empty' })
-    @MaxLength(20, {
-        message:
-          'First name is too long. Maximum length is $constraint1 characters.',
-      })
-    firstName: string;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'First name can not be empty' })
+  @MaxLength(20, {
+    message:
+      'First name is too long. Maximum length is $constraint1 characters.',
+  })
+  firstName: string;
 
-    @ApiProperty()
-    @IsNotEmpty({ message: 'Last name can not be empty' })
-    @MaxLength(20, {
-        message:
-          'Last name is too long. Maximum length is $constraint1 characters.',
-      })
-    lastName: string;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Last name can not be empty' })
+  @MaxLength(20, {
+    message:
+      'Last name is too long. Maximum length is $constraint1 characters.',
+  })
+  lastName: string;
 
-    @ApiProperty()
-    @IsNotEmpty({ message: 'The EIN can not be empty' })
-    @MaxLength(10, {
-        message:
-          'EIN is too long. Maximum length is $constraint1 characters.',
-      })
-    ein: string;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'The EIN can not be empty' })
+  @MaxLength(10, {
+    message: 'EIN is too long. Maximum length is $constraint1 characters.',
+  })
+  ein: string;
 
-    @ApiProperty()
-    @IsNotEmpty({ message: 'Email can not be empty' })
-    @MaxLength(30, {
-        message:
-          'Email is too long. Maximum length is $constraint1 characters.',
-      })
-    email: string;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Email can not be empty' })
+  @MaxLength(30, {
+    message: 'Email is too long. Maximum length is $constraint1 characters.',
+  })
+  email: string;
 
-    @ApiProperty()
-    @IsNotEmpty({ message: 'Description can not be empty' })
-    @MaxLength(4000, {
-        message:
-          'Description is too long. Maximum length is $constraint1 characters.',
-      })
-    description: string;
+  @ApiProperty()
+  @IsNotEmpty({ message: 'Description can not be empty' })
+  @MaxLength(4000, {
+    message:
+      'Description is too long. Maximum length is $constraint1 characters.',
+  })
+  description: string;
 }
--- src/request/dto/get-request.dto.ts
+++ src/request/dto/get-request.dto.ts
@@ -1,15 +1,25 @@
-import { ApiPropertyOptional, IntersectionType, PartialType, PickType } from '@nestjs/swagger';
+import {
+  ApiPropertyOptional,
+  IntersectionType,
+  PartialType,
+  PickType,
+} from '@nestjs/swagger';
 import { RequestStatus } from '../request.entity';
 import { IsEnum, IsOptional, IsPositive } from 'class-validator';
 import { CursorPaginationDto } from '../../common/dto/cursor-pagination.dto';
 import { CreateRequestDto } from './create-request.dto';
 
 export class GetRequestsDto extends IntersectionType(
-    CursorPaginationDto,
-    PartialType(
-      PickType(CreateRequestDto, ['name', 'firstName', 'lastName', 'email'] as const),
-    ),
-  ) {
+  CursorPaginationDto,
+  PartialType(
+    PickType(CreateRequestDto, [
+      'name',
+      'firstName',
+      'lastName',
+      'email',
+    ] as const),
+  ),
+) {
   @IsOptional()
   @ApiPropertyOptional()
   @IsEnum(RequestStatus)
