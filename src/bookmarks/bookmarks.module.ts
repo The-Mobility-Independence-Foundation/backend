04d419dad9e00a8625bffa2e8017dabe7182736e
aas1984 <aas1984@g.rit.edu>
2025-04-09 11:36:18 -0400

    refactor: removed unnecessary api endpoints

--- src/bookmarks/bookmarks.module.ts
+++ src/bookmarks/bookmarks.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { BookmarkService } from './bookmarks.service';
-import { BookmarkController } from './bookmarks.controller';
 import { Bookmark } from './bookmarks.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
@@ -15,7 +14,7 @@ import { CommonModule } from '../common/common.module';
     CommonModule,
   ],
   providers: [BookmarkService],
-  controllers: [BookmarkController],
+  controllers: [],
   exports: [BookmarkService],
 })
 export class BookmarkModule {}
